c7a33999cab370c464ddb6067d12f78d2ae964f3
Franklin <franklinblanco@tutanota.com>
2022-08-21 11:30:09 -0400

    Added error enum to dtos

---
+++ src/enums/error.rs
@@ -0,0 +1,8 @@
+
+pub enum Error {
+    CommunicatorError(String),
+    UknownError,
+    UnexpectedStatusCode(u16, u16, String),
+    NetworkError(String),
+    SerdeError
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@
 
 pub mod dtos;
 pub mod domain;
+pub mod enums;
