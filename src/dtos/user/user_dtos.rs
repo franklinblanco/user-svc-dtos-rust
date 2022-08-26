c4da3ee5708989cdd9bb9cc3c5bf2658d02d0daa
Franklin <franklinblanco@tutanota.com>
2022-08-26 13:09:52 -0400

    Added default app to userdtos

--- src/dtos/user/user_dtos.rs
+++ src/dtos/user/user_dtos.rs
@@ -2,6 +2,7 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct UserForCreationDto{
+    #[serde(default = "get_default_app")]
     pub app: String,
     pub email: String,
     pub password: String,
@@ -9,13 +10,19 @@ pub struct UserForCreationDto{
 }
 #[derive(Serialize, Deserialize, Debug)]
 pub struct UserForLoginDto{
+    #[serde(default = "get_default_app")]
     pub app: String,
     pub email: String,
     pub password: String
 }
 #[derive(Serialize, Deserialize, Debug)]
 pub struct UserForAuthenticationDto{
+    #[serde(default = "get_default_app")]
     pub app: String,
     pub email: String,
     pub token: String
+}
+
+fn get_default_app() -> String {
+    "deez".to_string()
 }
