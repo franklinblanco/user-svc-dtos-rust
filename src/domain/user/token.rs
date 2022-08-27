27fcb01ede4d52412e5d99d1a07d45bcc6f92011
Franklin <franklinblanco@tutanota.com>
2022-08-27 11:42:43 -0400

    Added serde renaming to fields that need it in token

--- src/domain/user/token.rs
+++ src/domain/user/token.rs
@@ -8,12 +8,15 @@ pub const REFRESH_TOKEN_EXPIRATION_TIME_IN_DAYS: i32 = 20;
 pub struct Token {
     #[serde(skip_serializing, skip_deserializing)]
     pub id: i32,
+    #[serde(rename = "userId")]
     pub user_id: i32,
     #[serde(skip_serializing, skip_deserializing)]
     pub time_created: Option<NaiveDateTime>,
     #[serde(skip_serializing, skip_deserializing)]
     pub last_updated: Option<NaiveDateTime>,
+    #[serde(rename = "authToken")]
     pub auth_token: String,
+    #[serde(rename = "refreshToken")]
     pub refresh_token: String
 }
 
